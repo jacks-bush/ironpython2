c617b1381e957c181099f48422bb486515d39466
Jackson Bush <jackson@hertzler.com>
2019-06-03 15:16:04 -0400

    Simplified reproducing the issue thanks to slozier

--- ReproduceError/Program.cs
+++ ReproduceError/Program.cs
@@ -1,75 +1,27 @@
-﻿using System.Threading;
+﻿using IronPython.Hosting;
 using Microsoft.Scripting.Hosting;
-using IronPython.Hosting;
-using IronPython.Runtime.Exceptions;
-using System.IO;
+using System.Collections.Generic;
 
 namespace ReproduceError {
     class Program {
-
         static void Main(string[] args) {
-
-            // perhaps try also adding a console?
             string mainCode = @"
-pyclass.ExecuteWithoutTracing()
+import sys
 
-if True:
-    if True:
-        if True:
-            if True:
-                basicClass = getBasicClass()
-                print basicClass.Hello
+def test(frame, event, args):
+    pass
+
+sys.settrace(test)
+
+class BasicClass:
+    pass
+
+BasicClass()
 ";
 
-            // doesn't fail every time so run in a loop until it does
-            while (true) {
-                // create engine and scope
-                ScriptEngine engine = Python.CreateEngine();
-                ScriptScope scope = engine.CreateScope();
-
-                // set initial tracing
-                engine.SetTrace(Debugger.OnTracebackReceived);
-
-                // set variable in engine
-                scope.SetVariable("pyclass", new PyClass(engine, scope));
-
-                // execute main code
-                engine.Execute(mainCode, scope);
-            }
-        }
-    }
-
-    public class Debugger {
-        public static TracebackDelegate OnTracebackReceived(TraceBackFrame frame, string result, object payload) {
-            return OnTracebackReceived;
-        }
-    }
-
-    public class PyClass {
-
-        private ScriptEngine _engine;
-        private ScriptScope _scope;
-
-        private const string code = @"
-def getBasicClass():
-	
-    class BasicClass:
-        pass
-    basic = BasicClass()
-    basic.Hello = ""Hello, cruel world""
-
-    return basic
-";
-
-        public PyClass(ScriptEngine engine, ScriptScope scope) {
-            _engine = engine;
-            _scope = scope;
-        }
-
-        public void ExecuteWithoutTracing() {
-            _engine.SetTrace(null);
-            _engine.Execute(code, _scope);
-            _engine.SetTrace(Debugger.OnTracebackReceived);
+            ScriptEngine engine = Python.CreateEngine(new Dictionary<string, object>() { { "CompilationThreshold", 0 } });
+            ScriptScope scope = engine.CreateScope();
+            engine.Execute(mainCode, scope);
         }
     }
 }
